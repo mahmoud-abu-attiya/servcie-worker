4a2943ffdd6b995dbe6ce411d51e37742b5b4586
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2022-11-14 00:00:24 +0200

    register service worker that sends a notification each day at 9 am

--- application.js
+++ application.js
@@ -1,67 +1,15 @@
 // application.js
-navigator.serviceWorker.register('./service-worker.js');
+// navigator.serviceWorker.register('./service-worker.js');
 
-// window.onload = async () => {
-//    const reg = await navigator.serviceWorker.getRegistration();
-//    Notification.requestPermission().then(permission => {
-//       if (permission !== 'granted') {
-//          alert('you need to allow push notifications');
-//       } else {
-//          const timestamp = new Date().getTime() + 5 * 1000; // now plus 5000ms
-//          // const timestamp = new Date().getTime(); // now plus 5000ms
-//          reg.showNotification(
-//             'Demo Push Notification',
-//             {
-//                tag: timestamp, // a unique ID
-//                body: 'Hello World', // content of the push notification
-//                // showTrigger: new TimestampTrigger(timestamp), // set the time for the push notification
-//                data: {
-//                   url: window.location.href, // pass the current url to the notification
-//                },
-//                badge: './icon.png',
-//                icon: './icon.png',
-//                actions: [
-//                   {
-//                      action: 'open',
-//                      title: 'Open app'
-//                   },
-//                   {
-//                      action: 'close',
-//                      title: 'Close notification',
-//                   }
-//                ]
-//             }
-//          );
-//       }
-//    });
-// };
-
-// navigator.serviceWorker.addEventListener('message', event => console.log(event.data));
-
-function showNotification() {
-   Notification.requestPermission((result) => {
-      if (result === 'granted') {
-         navigator.serviceWorker.ready.then((registration) => {
-            registration.showNotification('Vibration Sample', {
-               body: 'Buzz! Buzz!',
-               icon: 'icon.png',
-               vibrate: [200, 100, 200, 100, 200, 100, 200],
-               tag: 'vibration-sample',
-               actions: [
-                  {
-                     action: 'open',
-                     title: 'Open app'
-                  },
-                  {
-                     action: 'close',
-                     title: 'Close notification',
-                  }
-               ]
-            });
-         });
-      }
-   });
+const registerSw = async () => {
+    if ('serviceWorker' in navigator) {
+        Notification.requestPermission(async (result) => {
+            if (result === 'granted') {
+                await navigator.serviceWorker.register('service-worker.js');
+            }
+        })
+    };
 }
 
 
-showNotification()
+registerSw();
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,42 @@
-self.addEventListener('install', event => console.log('ServiceWorker installed'));
-
-
-self.addEventListener('open', event => {
-   event.waitUntil(self.clients.openWindow('/mahoud'));
-   console.log("open");
-   
+self.addEventListener('notificationclick', function (event) {
+   let url = 'https://belgravia.qa/admin/core/reservation/';
+   event.notification.close(); // Android needs explicit close.
+   event.waitUntil(
+      clients.matchAll({ type: 'window' }).then(windowClients => {
+         // Check if there is already a window/tab open with the target URL
+         for (var i = 0; i < windowClients.length; i++) {
+            var client = windowClients[i];
+            // If so, just focus it.
+            if (client.url === url && 'focus' in client) {
+               return client.focus();
+            }
+         }
+         // If not, then open the target URL in a new window/tab.
+         if (clients.openWindow) {
+            return clients.openWindow(url);
+         }
+      })
+   );
 });
 
-self.addEventListener('notificationclick', event => {
-   // event.waitUntil(self.clients.matchAll().then(clients => {
-   //    if (clients.length) { // check if at least one tab is already open
-   //       clients[0].focus();
-   //    } else {
-   //       self.clients.openWindow('/');
-   //    }
-   // }));
-   console.log("hi");
-});
 
-// self.addEventListener('notificationclick', event => {
-//    if (event.action === 'close') {
-//       event.notification.close();
-//    } else {
-//       self.clients.openWindow('/');
-//    }
-// });
+setInterval(async () => {
+   console.log("from service worker")
+   const start = 9 * 60; // starts at 9 am
+   const end = 10 * 60; // ends at 10 am
+   const date = new Date();
+   const now = date.getHours() * 60 + date.getMinutes();
+
+   if (start <= now && now <= end) {
+      try {
+         let res = await fetch("https://belgravia.qa/api/reservations/count/")
+         res = await res.json()
+         self.registration.showNotification("Today Reservations.", {
+            body: `We have ${res.count} reservations today.`,
+            icon: "./icon.png"
+         })
+      } catch (err) {
+         console.log("couldn't get reservations count")
+      }
+   }
+}, 60 * 60 * 1000)
